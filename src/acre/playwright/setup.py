c5abb03b37ade84be1f986c975a69c59c539cbae
Claudio Klingler <ck@realtime-projects.com>
2024-04-26 14:41:42 +0200

    Fix auto-setup.

--- src/acre/playwright/setup.py
+++ src/acre/playwright/setup.py
@@ -2,6 +2,6 @@ import subprocess
 
 
 def init():
-    rc = subprocess.run("playwright install").returncode
+    rc = subprocess.run("playwright install", shell=True).returncode
     if rc != 0:
         raise Exception("Failed to install playwright")
